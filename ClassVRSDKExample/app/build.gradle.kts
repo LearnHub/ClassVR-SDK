a73b1bb28986b6c35467c72802c328e43c886f07
Geraint Lloyd <geraint.lloyd@avantiseducation.com>
2024-04-17 07:23:00 +0100

    ClassVR SDK Example project - first commit

---
+++ ClassVRSDKExample/app/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+}
+
+android {
+    signingConfigs {
+        create("Avantis Signing Certificate") {
+            storeFile = file("E:\\Gwaith\\sign\\avantis_dev.keystore")
+            storePassword = "android"
+            keyAlias = "Learnpad"
+            keyPassword = "android"
+        }
+    }
+    namespace = "avn.classvrsdk.sample"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "avn.classvrsdk.sample"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        signingConfig = signingConfigs.getByName("Avantis Signing Certificate")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
