b6c7ac40dd3618c7c6e04fe7238cfdddf66bddee
Geraint Lloyd <geraint.lloyd@avantiseducation.com>
2024-04-18 15:18:10 +0100

    Fix to reload BLOB (i.e. png image) in Fragment.onResume()

--- ClassVRSDKExample/app/build.gradle.kts
+++ ClassVRSDKExample/app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "avn.classvrsdk.sample"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
--- ClassVRSDKExample/app/src/main/java/avn/classvrsdk/sample/ClassVRSDKFragment.kt
+++ ClassVRSDKExample/app/src/main/java/avn/classvrsdk/sample/ClassVRSDKFragment.kt
@@ -2,18 +2,18 @@ package avn.classvrsdk.sample
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import avn.classvrsdk.sample.databinding.FragmentFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
-
+class ClassVRSDKFragment : Fragment() {
+    private val TAG = ClassVRSDKFragment::class.java.name
     private var _binding: FragmentFirstBinding? = null
 
     // This property is only valid between onCreateView and
@@ -36,7 +36,15 @@ class FirstFragment : Fragment() {
         /*binding.buttonFirst.setOnClickListener {
             findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
         }*/
+    }
 
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called...")
         if (this.activity?.intent?.type.equals("application/x-lifeliqe")) {
             val bmp = BitmapFactory.decodeFile(this.activity?.intent?.data?.encodedPath)
             binding.imageView.setImageBitmap(bmp)
